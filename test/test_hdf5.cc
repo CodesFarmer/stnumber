9bb9b54b222bed74f684655d14536d50e887b2f3
CodesFarmer <jhonjoe.c@gmail.com>
2017-11-08 00:21:36 +0800

    HDF5

--- test/test_hdf5.cc
+++ test/test_hdf5.cc
@@ -18,19 +18,19 @@ int main(int argc, char * argv[]) {
     transfer->create_dataset(Mat2H5::DATA, data_dimension, "float");
     std::cout<<"Create Success! 2"<<std::endl;
     transfer->create_dataset(Mat2H5::LABEL, label_dimension, "float");
-    for(int iter = 0; iter < 1000; iter++) {
-        transfer->open_hdf5("test.h5");
-        cv::Mat image;
-        image = cv::imread("../matlab/samples/test_hdf5.png", CV_8UC1);
-        cv::transpose(image, image);
-        std::vector<cv::Mat> image_set;
-        image_set.push_back(image);
-        image_set.push_back(image);
-        std::cout << "The size of image is " << image.rows << std::endl;
-        transfer->write_data2hdf5(image_set);
-        float label[] = {1.0f, 3.0f, 2.0f, 4.0f, 9.0f, 8.0f, 6.0f, 7.0f};
-        transfer->write_label2hdf5(label, 2);
-    }
+//    for(int iter = 0; iter < 1000; iter++) {
+//        transfer->open_hdf5("test.h5");
+//        cv::Mat image;
+//        image = cv::imread("../matlab/samples/test_hdf5.png", CV_8UC1);
+//        cv::transpose(image, image);
+//        std::vector<cv::Mat> image_set;
+//        image_set.push_back(image);
+//        image_set.push_back(image);
+//        std::cout << "The size of image is " << image.rows << std::endl;
+//        transfer->write_data2hdf5(image_set);
+//        float label[] = {1.0f, 3.0f, 2.0f, 4.0f, 9.0f, 8.0f, 6.0f, 7.0f};
+//        transfer->write_label2hdf5(label, 2);
+//    }
     transfer->close_hdf5();
     return 0;
 }
