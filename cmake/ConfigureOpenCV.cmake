0da3f5c11eeddf36665d2184f38a6a33e4b578fd
sh_u <shenhui@untouch-tech.com>
2017-11-08 21:21:50 +0800

    retrained mtcnn

--- cmake/ConfigureOpenCV.cmake
+++ cmake/ConfigureOpenCV.cmake
@@ -1,11 +1,11 @@
-#set(CMAKE_PREFIX_PATH "/home/slam/software/opencv-3.1.0/build;${CMAKE_PREFIX_PATH}")
+set(CMAKE_PREFIX_PATH "$ENV{UNTOUCH_LOCAL}/Thirdparty/free/x86_64-linux/opencv-3.1.0/build;${CMAKE_PREFIX_PATH}")
 set(WITH_OPENCV ON "Compile with OpenCV")
 set(USE_OPENCV "Build with OpenCV support" ON)
 find_package(OpenCV)
 
 if(WITH_OPENCV)
     find_package(OpenCV REQUIRED)
-    list(APPEND DEST_INCLUDE_DIRS ${OpenCV_INCLUDE_DIRS})
+    list(APPEND ALL_INCLUDE_DIRS ${OpenCV_INCLUDE_DIRS})
     list(APPEND DEST_LINK_TARGETS ${OpenCV_LIBRARIES})
     message(STATUS "Compile with OpenCV")
 else()
--- src/detection/hand_boundingbox.cc
+++ src/detection/hand_boundingbox.cc
@@ -6,24 +6,30 @@
 #include "detect_face.h"
 
 extern "C" {
-    boost::shared_ptr<FaceDetector<float> > detector;
-    int initialize_detector(const std::map<std::string, std::pair<std::string, std::string> > &modelpath) {
-        std::vector<float> mean_value(1, 17.2196);
-        float img2net_scale = 0.0125;
-        detector = boost::make_shared<FaceDetector<float> >();
-        if(detector->initialize_network(modelpath)<0) {
-            return -1;
-        }
-        detector->initialize_transformer(img2net_scale, mean_value);
+boost::shared_ptr<FaceDetector<float> > detector;
+int initialize_detector(const std::map<std::string, std::pair<std::string, std::string> > &modelpath) {
+    std::vector<float> mean_value(1, 17.2196);
+    float img2net_scale = 0.0125;
+    detector = boost::make_shared<FaceDetector<float> >();
+    if(detector->initialize_network(modelpath)<0) {
+        return -1;
     }
-    //Get the bounding box
-    cv::Rect get_hand_bbx(const cv::Mat &image) {
-        cv::Mat tmpimg = image.clone();
-        std::vector<std::vector<float> > hand_bbx = detector->detect_face(tmpimg);
-        int x_l = std::max( hand_bbx[0][0], 0.0f );
-        int y_l = std::max( hand_bbx[0][1], 0.0f);
-        int x_r = std::min( hand_bbx[0][2], float(image.cols-1) );
-        int y_r = std::min( hand_bbx[0][3], float(image.rows-1) );
-        return cv::Rect(x_l, y_l, x_r - x_l + 1, y_r - y_l + 1);
-    }
+    detector->initialize_transformer(img2net_scale, mean_value);
+}
+//Get the bounding box
+cv::Rect get_hand_bbx(const cv::Mat &image) {
+    cv::Mat tmpimg = image.clone();
+    std::vector<std::vector<float> > hand_bbx = detector->detect_face(tmpimg);
+    cv::Rect hand_rect(-1, -1, -1, -1);
+    if(hand_bbx.size() == 0) return hand_rect;
+    int x_l = std::max( hand_bbx[0][0], 0.0f );
+    int y_l = std::max( hand_bbx[0][1], 0.0f);
+    int x_r = std::min( hand_bbx[0][2], float(image.cols-1) );
+    int y_r = std::min( hand_bbx[0][3], float(image.rows-1) );
+    hand_rect.x = x_l;
+    hand_rect.y = y_l;
+    hand_rect.width = x_r - x_l + 1;
+    hand_rect.height = y_r - y_l + 1;
+    return hand_rect;
+}
 }
